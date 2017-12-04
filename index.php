4686968f63598fa22ad9be2afe093ce063180c37
fedjensa <jens.hedberg@medieinstitutet.se>
2017-12-04 03:12:32 +0100

    testing basic layout on landing page

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 
-<div class="container">
+<div class="container-fluid header">
   <div class="row">
    <div class="col-12">
        <h1 class="test">Hello</h1>
@@ -17,6 +17,56 @@
     </div>
   </div>
 </div>
+<div class="container-fluid info">
+    <div class="row">
+        <div class="col-12">
+           <h1 class="test">Hello</h1>
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+    </div>
+</div>
+<div class="container-fluid news">
+    <div class="row">
+        <div class="col-12">
+           <h1 class="test">Hello</h1>
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+        <div class="col-sm">
+          One of three columns
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="container-fluid footer">
+        <div class="row">
+            <div class="col-12">
+               <h1 class="test">Hello</h1>
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+            <div class="col-sm">
+              One of three columns
+            </div>
+        </div>
+    </div>
+</footer>
 
 
 <?php get_footer(); ?>
