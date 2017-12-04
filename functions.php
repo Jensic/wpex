3f43adf3260ca4b3dea550517cb12744ae350cd5
fedjensa <jens.hedberg@medieinstitutet.se>
2017-12-04 02:16:57 +0100

    added menu, support for bootstrap menu

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function ex_files() {
-  //wp_enqueue_script('ex-js', get_theme_file_uri('/js/all.js'), NULL, microtime(), true);
+    //wp_enqueue_script('ex-js', get_theme_file_uri('/js/all.js'), NULL, microtime(), true);
     wp_enqueue_script('all', get_template_directory_uri() . '/js/all.js', array ('jquery' ), null, microtime(), true);
     
     wp_enqueue_script('popper_js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js');
@@ -12,9 +12,12 @@ function ex_files() {
     
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     
-  //wp_enqueue_style('ex_styles', get_stylesheet_uri());
+    //wp_enqueue_style('ex_styles', get_stylesheet_uri());
     wp_enqueue_style('bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css');
     wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css', false, microtime(), 'all');
+    
+    // Register Custom Navigation Walker
+    require_once('class-wp-bootstrap-navwalker.php');
     
 }
 
@@ -22,6 +25,30 @@ add_action('wp_enqueue_scripts', 'ex_files');
 
 function ex_features() {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+    add_theme_support('post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'video', 'audio'));
 }
 
 add_action('after_setup_theme', 'ex_features');
+
+function ex_menus() {
+    register_nav_menus( array(
+    	'primary' => __('Primary Menu', 'ex'),
+	));
+}
+
+add_action('init', 'ex_menus');
+
+function bootstrap_nav() {
+    wp_nav_menu(array(
+        'theme_location'    => 'primary',
+        'depth'             => 2,
+        'container'         => 'div',
+        'container_class'   => 'collapse navbar-collapse',
+        'container_id'      => 'navbar',
+        'menu_class'        => 'navbar-nav mr-auto',
+        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+        'walker'            => new WP_Bootstrap_Navwalker())
+    );
+    }
--- header.php
+++ header.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">WT</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php bootstrap_nav(); ?>
+        </nav>
