04e044c6b77348dc7722fc7c448a5aca33715b21
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-06 06:26:44 +0100

    styling of front page

--- template-parts/info.php
+++ template-parts/info.php
@@ -1,25 +1,25 @@
 <div class="container-fluid info">
     <div class="container">
-        <div class="row">
-            <div class="col-sm info__container">
-                <span class="fa fa-lightbulb-o info__container__icon--yellow" aria-hidden="true"></span>
-                <div class="info__container__text">
-                    <p class="info__container__text--main">One of three columns</p>
-                    <p class="info__container__text--sub">Some text go here</p>
+        <div class="row info__container">
+            <div class="col-sm info__container__content">
+                <span class="fa fa-lightbulb-o info__container__content__icon--yellow" aria-hidden="true"></span>
+                <div class="info__container__content__text">
+                    <p class="info__container__content__text--main">One of three columns</p>
+                    <p class="info__container__content__text--sub">Some text go here</p>
                 </div>
             </div>
-            <div class="col-sm info__container">
-                <span class="fa fa-lightbulb-o info__container__icon--green" aria-hidden="true"></span>
-                <div class="info__container__text">
-                    <p class="info__container__text--main">One of three columns</p>
-                    <p class="info__container__text--sub">Some text go here</p>
+            <div class="col-sm info__container__content">
+                <span class="fa fa-lightbulb-o info__container__content__icon--green" aria-hidden="true"></span>
+                <div class="info__container__content__text">
+                    <p class="info__container__content__text--main">One of three columns</p>
+                    <p class="info__container__content__text--sub">Some text go here</p>
                 </div>
             </div>
-            <div class="col-sm info__container">
-                <span class="fa fa-lightbulb-o info__container__icon--pink" aria-hidden="true"></span>
-                <div class="info__container__text">
-                    <p class="info__container__text--main">One of three columns</p>
-                    <p class="info__container__text--sub">Some text go here</p>
+            <div class="col-sm info__container__content">
+                <span class="fa fa-lightbulb-o info__container__content__icon--pink" aria-hidden="true"></span>
+                <div class="info__container__content__text">
+                    <p class="info__container__content__text--main">One of three columns</p>
+                    <p class="info__container__content__text--sub">Some text go here</p>
                 </div>
             </div>
         </div>
