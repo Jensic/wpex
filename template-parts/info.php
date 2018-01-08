8f688697997366fb9d5607043d18234acb181ecc
fedjensa <jens.hedberg@medieinstitutet.se>
2018-01-08 03:54:16 +0100

    styling of front page, testing parallax

--- template-parts/info.php
+++ template-parts/info.php
@@ -4,22 +4,22 @@
             <div class="col-sm info__container__content">
                 <span class="fa fa-lightbulb-o info__container__content__icon--yellow" aria-hidden="true"></span>
                 <div class="info__container__content__text">
-                    <p class="info__container__content__text--main">One of three columns</p>
-                    <p class="info__container__content__text--sub">Some text go here</p>
+                    <p class="info__container__content__text--main">Companies</p>
+                    <p class="info__container__content__text--sub">More then 80</p>
                 </div>
             </div>
             <div class="col-sm info__container__content">
                 <span class="fa fa-lightbulb-o info__container__content__icon--green" aria-hidden="true"></span>
                 <div class="info__container__content__text">
-                    <p class="info__container__content__text--main">One of three columns</p>
-                    <p class="info__container__content__text--sub">Some text go here</p>
+                    <p class="info__container__content__text--main">People</p>
+                    <p class="info__container__content__text--sub">Over 300</p>
                 </div>
             </div>
             <div class="col-sm info__container__content">
                 <span class="fa fa-lightbulb-o info__container__content__icon--pink" aria-hidden="true"></span>
                 <div class="info__container__content__text">
-                    <p class="info__container__content__text--main">One of three columns</p>
-                    <p class="info__container__content__text--sub">Some text go here</p>
+                    <p class="info__container__content__text--main">Networking</p>
+                    <p class="info__container__content__text--sub">Road to success</p>
                 </div>
             </div>
         </div>
